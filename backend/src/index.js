b337d235217a2cb7de154cf2aa131100fb37040f
jdav892 <jadavila9@gmail.com>
2025-01-02 13:57:45 -0500

    feat: creates function to generate jwt for users

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use(express.json());
 
 app.listen(PORT, () => {
    console.log(`Server is running on ${PORT}`);
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+        expiresIn:"7d"
+    });
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, //7 days represented as MS
+        httpOnly: true, //Guarantees access only by http request to prevent xss attacks
+        sameSite: "strict", //to prevent csrf attack
+        secure: process.env.NODE_ENV !== "development"
+    });
+    return token;
+};
+
+
+    
