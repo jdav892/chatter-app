3c16ab4cb27bd43e7f4a03eb377e56167e531157
jdav892 <jadavila9@gmail.com>
2025-01-03 12:48:47 -0500

    feat: completes message controller with json message error rather than json error will see if needs to be changed

--- backend/src/routes/messageRoute.js
+++ backend/src/routes/messageRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/authMiddleware.js";
-import { getUsersForSidebar } from "../controllers/messageController.js";
+import { getUsersForSidebar, getMessages } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 
 export default router;
