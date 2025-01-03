0012f726830e40e2809f08d2450e8b7f52623e1a
jdav892 <jadavila9@gmail.com>
2025-01-03 01:52:45 -0500

    feat: finishes login controller function

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -48,14 +48,27 @@ export const login = async (req, res) => {
     const { email, password } = req.body
 
     try {
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email });
 
         if(!user) {
-            return res.status(400).json({message: "Invalid credentials"})
+            return res.status(400).json({message: "Invalid credentials"});
         }
 
-    }catch (error){
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        generateToken(user._id, res);
 
+        res.stats(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic, 
+        });
+    } catch (error){
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: "Internal Server Error"});
     }
 };
 
