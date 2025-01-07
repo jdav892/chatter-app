915cc59201761839b126285228ff71db8313c324
jdav892 <jadavila9@gmail.com>
2025-01-06 19:16:18 -0500

    feat: creates theme store to be imported to create themes in app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,14 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from  ".store/useThemeStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-tost"
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth } = useAuthStore();
-  
+  const { theme } useThemeStore 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
