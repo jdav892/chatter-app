908de3617fe34e3faf8b4e37b5da3ee4d341ade4
jdav892 <jadavila9@gmail.com>
2025-01-06 20:19:44 -0500

    feat: creates nav bar component for displayed pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,14 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
-import { useThemeStore } from  ".store/useThemeStore";
+import { useThemeStore } from  "./store/useThemeStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
-import { Toaster } from "react-hot-tost"
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth } = useAuthStore();
-  const { theme } useThemeStore 
+  const { theme } = useThemeStore 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
