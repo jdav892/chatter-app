8b7bd8826b25270270ccab31e6b2404bf7107e7e
jdav892 <jadavila9@gmail.com>
2025-01-10 14:57:00 -0500

    feat: builds app and changes ports for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: `http://localhost:8080/api`,
+    baseURL: import.meta.env.MODE === "development" ? `http://localhost:8080/api` : "/api",
     withCredentials: true,
 });
 
